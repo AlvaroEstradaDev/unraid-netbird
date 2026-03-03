e012bfc128d1bb54ed588313018f8bd7e425075a
Alvaro Estrada <alvaro.jesus@estradajai.me>
2026-03-03 12:24:52 -0600

    fix: remove auto-refresh to prevent dashboard crash

--- src/usr/local/emhttp/plugins/netbird/include/data/Dashboard.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Netbird;
-
-$docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
-require_once "{$docroot}/plugins/netbird/include/page.php";
-
-echo getPage("Dashboard", false);
